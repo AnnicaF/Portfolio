c5d8033b40d54dfdb4d2b0ebfe5ff9617bfeab4c
AnnicaF <95623021+AnnicaF@users.noreply.github.com>
2022-10-05 20:42:29 +0200

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,54 @@
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+  portfolioModals[modalClick].classList.add("active");
+
+}
+
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener("click", () => {
+    portfolioModal(i);
+  });
+
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalView) => {
+      portfolioModalView.classList.remove("active");
+
+    });
+
+  });
+
+});
+
+
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav.items a");
+
+menuBtn.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navigation.classList.remove("active");
+  });
+
+});
